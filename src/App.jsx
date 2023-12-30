640482bb0080e04d124fad33b18f2ce43da2f8da
Shagufta <shaguftanaureen0506@gmail.com>
2023-12-31 00:16:00 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+
+import './App.css'
+// import Spline from "@splinetool/react-spline"
+// import { useState } from 'react'
+
+function App() {
+  return (
+    <main className="app-container">
+      <iframe
+        // className='h-screen'
+        src='https://my.spline.design/untitled-6ae2fd7be49f326702571524adea3457/'
+        width="100%"
+        height="100%"
+      >
+      </iframe>
+    </main>
+  )
+}
+
+export default App
